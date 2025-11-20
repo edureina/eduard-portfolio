e2f283cc068d13e1ae32bfdee5bd30c2b34df17a
Eduard <eduard.reina@gmail.com>
2025-11-20 17:35:10 +0100

    Updated to publish in github

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: '/eduard-portfolio/',   //  MUST MATCH my repo name exactly
 })
